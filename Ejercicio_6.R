3a2078482e56e833e0148c71acfb1ace5bbedc25
GJMedina5 <gabrieljosemedina55@gmail.com>
2026-03-11 19:19:39 -0300

    Add files via upload

---
+++ Ejercicio_6.R
@@ -0,0 +1,11 @@
+# Ejercicio 6 - Metadatos en R
+
+titulo <- "Ejercicios del módulo 1"
+autor <- "Gabriel José Medina"
+fecha <- Sys.Date()
+materia <- "Ingeniería Mecatrónica"
+
+print(titulo)
+print(autor)
+print(fecha)
+print(materia)
